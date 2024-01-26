d660d8d767fa5112e3ec59203d4e0bedcce8aca2
Murilo Iachitzke <miachitzke@mail.com>
2024-01-26 17:27:40 -0300

    função principal adicionada

---
+++ src/scripts/main.js
@@ -0,0 +1,59 @@
+const inputJson = document.getElementById("json-input");
+const visualizationContainer = document.getElementById(
+  "visualization-container");
+
+inputJson.addEventListener("input", function () {
+  const jsonString = inputJson.value.trim();
+
+  try {
+    const jsonObject = JSON.parse(jsonString);
+    renderObject(jsonObject, visualizationContainer);
+  } catch (error) {
+    visualizationContainer.innerHTML = '<p class="error">JSON inválido</p>';
+  }
+});
+
+function renderObject(obj, container) {
+  container.innerHTML = ""; 
+
+  const rootBox = document.createElement("div");
+  rootBox.classList.add("box");
+  rootBox.innerHTML = "<strong>Objeto Raiz:</strong><br/>";
+  container.appendChild(rootBox);
+
+  renderProperties(obj, rootBox);
+}
+
+function renderProperties(obj, container) {
+  for (const key in obj) {
+    let adjustedKey = key;
+    const value = obj[key];
+
+    if (/^\d+$/.test(key)) {
+      adjustedKey = String(parseInt(key, 10) + 1);
+    }
+
+    const propertyBox = document.createElement("div");
+    propertyBox.classList.add("box");
+    propertyBox.innerHTML = `<strong>${adjustedKey}</strong> `;
+
+    if (typeof value === "object" && value !== null) {
+      // Se o valor é um objeto, renderiza suas propriedades recursivamente
+      propertyBox.classList.add("box-objeto");
+
+      const numMembers = Object.keys(value).length;
+
+      if (numMembers > 0) {
+        propertyBox.innerHTML += `(${numMembers}): `;
+      }
+
+      renderProperties(value, propertyBox);
+    } else {
+      propertyBox.classList.add("box-valor", "editable");
+      propertyBox.contentEditable = "true";
+      propertyBox.innerText += value;
+    }
+
+    container.appendChild(propertyBox);
+  }
+}
